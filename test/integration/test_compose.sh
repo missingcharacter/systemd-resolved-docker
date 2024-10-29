94371161759c8299b23c3802cb5e91a1c719fd47
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2024-10-28 21:01:59 -0500

    [test_compose.sh] added `echo` as debug messages

--- test/integration/test_compose.sh
+++ test/integration/test_compose.sh
@@ -46,13 +46,24 @@ networks:
       driver: default
 EOF
 
+echo 'Before ALLOWED_DOMAINS'
+
 ALLOWED_DOMAINS=.docker,.$TEST_PREFIX start_systemd_resolved_docker
+
+echo 'After ALLOWED_DOMAINS'
+echo 'Before first docker compose'
 
 docker compose --file /dev/fd/10 --project-name $TEST_PREFIX up --detach --scale webserver=2
 
+echo 'After first docker compose'
+echo "Before docker_ip ${TEST_PREFIX}-broker-1"
+
 broker1_ip=$(docker_ip ${TEST_PREFIX}-broker-1)
+echo "broker1_ip is ${broker1_ip}"
 webserver1_ip=$(docker_ip ${TEST_PREFIX}-webserver-1)
+echo "webserver1_ip is ${webserver1_ip}"
 webserver2_ip=$(docker_ip ${TEST_PREFIX}-webserver-2)
+echo "webserver2_ip is ${webserver2_ip}"
 
 query_ok     broker.$TEST_PREFIX $broker1_ip
 query_ok   1.broker.$TEST_PREFIX $broker1_ip
@@ -64,5 +75,10 @@ query_ok   2.webserver.$TEST_PREFIX $webserver2_ip
 
 query_ok     broker.docker $broker1_ip
 
+echo 'Before second docker compose'
+
 docker compose --file /dev/fd/20 --project-name ${TEST_PREFIX}_2 up --detach
+
+echo 'After second docker compose'
+
 query_fail   broker.docker
